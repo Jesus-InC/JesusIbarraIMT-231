5e64cfe95517b1332475eb39529ffd987e90a98f
Jesus-InC <jesus.ibarra@ucb.edu.bo>
2025-02-25 12:25:10 -0400

    add practica1 y comandos

---
+++ Tarea1/practica1.sh
@@ -0,0 +1,15 @@
+#bin/bash
+
+#Mostrar el directorio actual
+pwd
+#Acceder a la carpeta
+cd /tmp
+#Listar su contenido
+ls
+#Volver al directorio personal
+cd ~
+#Crear carpeta
+mkdir practica_shell
+#Mostrar el directorio actual
+pwd
+
