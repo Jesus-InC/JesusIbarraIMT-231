e046898d9b4e0d496b1172ef1d9da31d1f101de5
Jesus-InC <jesus.ibarra@ucb.edu.bo>
2025-03-03 22:26:36 -0400

    add practica3

---
+++ Tarea1/practica3.sh
@@ -0,0 +1,13 @@
+#bin/bash
+
+#Creación de los tres directorios
+mkdir documentos imagenes scripts
+#Creación de archivos dentro de documentos
+cd documentos
+touch nota1.txt nota2.txt nota3.txt nota4.txt nota5.txt
+#Mover archivos a backup
+mv nota3.txt ~/practica_shell/backup
+mv nota4.txt ~/practica_shell/backup
+#Eliminar la carpeta imagenes
+cd ..
+rm -r imagenes
