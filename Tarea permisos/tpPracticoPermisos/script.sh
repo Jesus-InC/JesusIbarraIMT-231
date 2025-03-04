e90ca2f82acd6b5de6d524e6e6d03be74eb7ab01
Jesus-InC <jesus.ibarra@ucb.edu.bo>
2025-03-04 14:57:55 -0400

    add tareaPermisos

---
+++ Tarea permisos/tpPracticoPermisos/script.sh
@@ -0,0 +1,8 @@
+#bin/bash
+
+#Imprimir el mensaje
+echo "Por favor, ingrese su nombre"
+#Leer la respuesta
+read nombre
+#Imprimir el saludo junto con la respuesta leída
+echo "Mucho gusto," $nombre". ¿Cómo estás?"
